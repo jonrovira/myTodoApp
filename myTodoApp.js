3bfb306c84e45568fac6e8dc95caea2dc55fea0c
Jon Rovira <jonathanrovira2016@u.northwestern.edu>
2014-11-15 17:49:25 -0600

    commitments addable to commitments collection

--- myTodoApp.js
+++ myTodoApp.js
@@ -5,7 +5,24 @@ if (Meteor.isClient) {
 
     Template.body.helpers({
         commitments: function() {
-            return Commitments.find({});
+            return Commitments.find({}, {sort: {createdAt: -1}});
+        }
+    });
+    Template.addCommitment.events({
+        "submit form": function (event) {
+
+            // Get new commitment name
+            var text = event.target.name.value;
+
+            // Add it to commitment collection
+            Commitments.insert({
+                name: text,
+                createdAt: new Date() // current time
+            });
+
+            // Cleanup: Clear input, void default submit
+            event.target.name.value = "";
+            return false;
         }
     });
 }
