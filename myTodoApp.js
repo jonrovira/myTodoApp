e52200b52d09bac7094ad569f68fd71fad494769
Jon Rovira <jonathanrovira2016@u.northwestern.edu>
2014-11-15 18:35:51 -0600

    can add tasks to commitments, but not to specifiic commitments yet

--- myTodoApp.js
+++ myTodoApp.js
@@ -1,36 +1,65 @@
 Commitments = new Mongo.Collection("commitments");
+Tasks       = new Mongo.Collection("tasks");
 
 
 if (Meteor.isClient) {
 
+    /* body */
     Template.body.helpers({
         commitments: function() {
             return Commitments.find({}, {sort: {createdAt: -1}});
         }
     });
+
+    /* addCommitment */
     Template.addCommitment.events({
         "submit form": function(event) {
 
-            // Get new commitment name
-            var text = event.target.name.value;
+            // Get new commitment string
+            var text = event.target.commitment.value;
 
-            // Add it to commitment collection
+            // Add commitment to commitments collection
             Commitments.insert({
-                name: text,
-                createdAt: new Date() // current time
+                commitment: text,
+                createdAt: new Date()
             });
 
-            // Cleanup: Clear input, void default submit
-            event.target.name.value = "";
+            // Cleanup: clear input, void default submit
+            event.target.commitment.value = "";
             return false;
+        }
+    });
+
+    /* addTask */
+    Template.addTask.events({
+        "submit form": function(event) {
+
+            // Get new task string
+            var task = event.target.task.value;
+
+            // Add task to tasks collection
+            Tasks.insert({
+                task: task,
+                createdAt: new Date() 
+            });
+
+            // Cleanup: clear input, void default submit
+            event.target.task.value = "";
+            return false;
+        }
+    });
+
+    /* commitment */
+    Template.commitment.helpers({
+        tasks: function() {
+            return Tasks.find({}, {sort: {createdAt: -1}});
         }
     });
     Template.commitment.events({
         "click .toggle-checked": function() {
             Commitments.update(this._id, {$set: {checked: ! this.checked}});
         },
-        "click .delete": function() {
-            console.log('hi');
+        "click .delete": function() {   
             Commitments.remove(this._id);
         }
     });
